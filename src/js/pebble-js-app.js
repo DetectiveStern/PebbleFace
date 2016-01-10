ed3201c56bc42f21eda031d0c05a0de659a86254
DetectiveStern <DetectiveStern@users.noreply.github.com>
2016-01-10 17:21:51 -0500

    Added API Requirements

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -9,8 +9,9 @@ var xhrRequest = function (url, type, callback) {
 
 function locationSuccess(pos) {
   // Construct URL
+	var apiKey = 'bc9804685215e004b3f6cf602d4d9914';
   var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
-      pos.coords.latitude + "&lon=" + pos.coords.longitude;
+      pos.coords.latitude + "&lon=" + pos.coords.longitude + "&appid=" + apiKey;
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET', 
